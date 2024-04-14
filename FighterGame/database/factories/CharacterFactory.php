90f86f12ca1c0dbf3b1aa3c4e0bdae34dea59526
Gáspár Róbert <hvh9yf@inf.elte.hu>
2024-04-14 22:28:12 +0200

    Factory

---
+++ FighterGame/database/factories/CharacterFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Character>
+ */
+class CharacterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $defence = fake()->numberBetween(0, 3);
+        $strength = fake()->numberBetween(0, 20 - $defence);
+        $accuracy = fake()->numberBetween(0, 20 - ($defence + $strength));
+        $magic = 20 - ($defence + $strength + $accuracy);
+        return [
+        'name' => fake()->name(),
+        'defence' => $defence,
+        'strength' => $strength,
+        'accuracy' => $accuracy,
+        'magic' => $magic,
+        ];
+    }
+}
