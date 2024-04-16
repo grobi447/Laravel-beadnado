d15cac83cd39dc0045a9fd284eb3afaf1dfdd1d0
Gáspár Róbert <hvh9yf@inf.elte.hu>
2024-04-16 13:42:11 +0200

    CharacterListing

---
+++ FighterGame/resources/views/character/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="w-max">
+                <div class="card">
+                    <div class="card-header ">
+                        <h2>My Characters</h2>
+                    </div>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="container">
+                            <div class="row ">
+                                @foreach($characters as $character)
+                                <div class="col-md-6">
+                                    <!-- Ide jön a karakter specifikus tartalom -->
+                                    <div class="card">
+                                        <div class="card-header text-center">
+                                            {{ $character->name }}
+                                        </div>
+                                        <div class="card-body text-center">
+                                            <div class="container">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        Defence: {{$character->defence}}
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        Strength: {{$character->strength}}
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        Accuracy: {{$character->accuracy}}
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        Magic: {{$character->magic}}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                                @endforeach
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    @endsection
--- FighterGame/routes/web.php
+++ FighterGame/routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\CharacterController;
 
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/characters', [CharacterController::class, 'index'])->middleware('auth');
 
 Auth::routes();
 
